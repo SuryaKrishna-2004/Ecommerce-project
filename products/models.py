79275888863286c9784d2a6873a0138cb5ceb2aa
SuryaKrishna-2004 <suryakrishna2004feb13@gmail.com>
2026-03-09 19:13:56 +0300

    model created

--- products/models.py
+++ products/models.py
@@ -3,8 +3,20 @@ from django.db import models
 # model for product
 
 class Product(models.Models):
+
+    LIVE=1
+    DELETE=0
+    DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
     title=models.CharField(max_length=200)
     price=models.FloatField()
     description=models.TextField()
-    image=models.ImageField()
+    image=models.ImageField(upload_to='/media')
+    priority=models.IntegerField(default=0)
+    delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
+    created_at=models.DateTimeField(auto_now_add= True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
     
