0ee86f46f23462132855bf1404b2d9f973b0d59f
SuryaKrishna-2004 <suryakrishna2004feb13@gmail.com>
2026-03-09 19:01:09 +0300

    model creation

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# model for product
+
+class Product(models.Models):
+    title=models.CharField(max_length=200)
+    price=models.FloatField()
+    description=models.TextField()
+    image=models.ImageField()
+    
